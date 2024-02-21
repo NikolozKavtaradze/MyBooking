6fd2763f31c4d5acc6bd8210ae5d6453c6c626d8
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-21 13:35:59 -0800

    Added Custom errors for Apartment and User

---
+++ MyBooking.Domain/Users/UserErrors.cs
@@ -0,0 +1,14 @@
+﻿using MyBooking.Domain.Abstractions;
+
+namespace MyBooking.Domain.Users;
+
+public static class UserErrors
+{
+    public static readonly Error NotFound = new(
+        "User.NotFound",
+        "The user with the specified identifier was not found");
+
+    public static readonly Error InvalidCredentials = new(
+        "User.InvalidCredentials",
+        "The provided credentials were invalid");
+}
