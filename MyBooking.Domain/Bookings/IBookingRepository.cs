7f43b3b6c0e81eb1c251c4adf4b633e107fb8863
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-21 13:36:18 -0800

    Added IBookingRepository

---
+++ MyBooking.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyBooking.Domain.Apartments;
+
+namespace MyBooking.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
+}
