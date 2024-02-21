926ab4708e9db1fefa79293fa84862ee64366887
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-21 13:36:47 -0800

    Added required abstractions for CQRS

---
+++ MyBooking.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MyBooking.Domain.Abstractions;
+
+namespace MyBooking.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand,Result>
+    where TCommand : ICommand
+{
+    
+}
+
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+    
+}
