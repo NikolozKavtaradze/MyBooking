6bee63493a863e7d97483fc2b856002febfb484d
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-23 06:18:59 -0800

    Added GetBooking Query and its handler with Dapper

---
+++ MyBooking.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,5 @@
+﻿using MyBooking.Application.Abstractions.Messaging;
+
+namespace MyBooking.Application.Bookings.GetBooking;
+
+public record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
