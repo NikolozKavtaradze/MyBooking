c03fcfd103a487238fe6d9c98f7390bba7a076b0
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-25 08:33:00 -0800

    Added Ef core dbcontext and Added Dependency injection class where we registered:  Email and Datetime Services, DbContext which uses postgres and also ef core snake case naming convention

---
+++ MyBooking.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBooking.Domain.Abstractions;
+
+namespace MyBooking.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+
+    }
+}
