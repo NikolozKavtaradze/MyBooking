724909c44add5024f5ff2cd20e49c0ac19f51209
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-25 08:31:20 -0800

    Implemented Clock and Email Service

---
+++ MyBooking.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using MyBooking.Application.Abstractions.Clock;
+
+namespace MyBooking.Infrastructure.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
---
+++ MyBooking.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using MyBooking.Application.Abstractions.Email;
+
+namespace MyBooking.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
