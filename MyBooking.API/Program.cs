75d6c071095b73d9c9ec83789115b347f602874f
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-27 05:10:06 -0800

    Added connection string for postgress

--- MyBooking.API/Program.cs
+++ MyBooking.API/Program.cs
@@ -1,9 +1,16 @@
+using MyBooking.Application;
+using MyBooking.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
