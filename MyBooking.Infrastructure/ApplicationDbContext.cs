47b30de701f33cc2507af9a4b38e0deebc6e5f65
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-27 03:27:59 -0800

    Added Publishing Domain Events with mediator publisher when we call SaveChangesAsync

--- MyBooking.Infrastructure/ApplicationDbContext.cs
+++ MyBooking.Infrastructure/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyBooking.Domain.Abstractions;
+using System.Data;
+using MediatR;
 
 namespace MyBooking.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options)
+    private readonly IPublisher _publisher;
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
-
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -16,5 +19,37 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
