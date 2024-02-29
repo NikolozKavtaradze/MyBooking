19a62a81c16245f9c59ed406ac27c912e9309a1a
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-02-29 12:12:59 -0800

    Abstraction for Authentication service

---
+++ MyBooking.Application/Abstractions/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using MyBooking.Domain.Users;
+
+namespace MyBooking.Application.Abstractions;
+
+public interface IAuthenticationService
+{
+    Task<string> RegisterAsync(
+        User user, 
+        string password,
+        CancellationToken cancellationToken = default);
+}
