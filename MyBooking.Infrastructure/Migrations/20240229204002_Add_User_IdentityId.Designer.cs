e742242dbaf183d6ee19a7474da1a4d6df3c0136
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-01 05:37:14 -0800

    Added Migrations for User Table to add identityId, which is taken from keycloak

--- MyBooking.Infrastructure/Migrations/20240229204002_Add_User_IdentityId.Designer.cs
+++ MyBooking.Infrastructure/Migrations/20240229204002_Add_User_IdentityId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBooking.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyBooking.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240229204002_Add_User_IdentityId")]
+    partial class Add_User_IdentityId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
