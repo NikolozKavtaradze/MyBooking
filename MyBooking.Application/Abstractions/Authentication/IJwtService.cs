d43e6a7e55d727af6c565b06cab234dd0c6cfb9d
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-01 05:38:41 -0800

    jwt service for fetching access token

---
+++ MyBooking.Application/Abstractions/Authentication/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using MyBooking.Domain.Abstractions;
+
+namespace MyBooking.Application.Abstractions.Authentication;
+
+public interface IJwtService
+{
+    Task<Result<string>> GetAccessTokenAsync(
+        string email,
+        string password,
+        CancellationToken cancellationToken = default);
+}
