0c43d57bc2497875df8ee38878a11c6105566296
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-01 05:39:08 -0800

    Login user command command handler, and specific response record type

---
+++ MyBooking.Application/Users/LoginUser/LogInUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using MyBooking.Application.Abstractions.Messaging;
+using MyBooking.Application.Users.LogInUser;
+
+namespace MyBooking.Application.Users.LogInUser;
+
+public sealed record LogInUserCommand(string Email, string Password)
+    : ICommand<AccessTokenResponse>;
---
+++ MyBooking.Application/Users/LoginUser/LogInUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MyBooking.Application.Abstractions.Authentication;
+using MyBooking.Application.Abstractions.Messaging;
+using MyBooking.Domain.Abstractions;
+using MyBooking.Domain.Users;
+
+namespace MyBooking.Application.Users.LogInUser;
+
+internal sealed class LogInUserCommandHandler : ICommandHandler<LogInUserCommand, AccessTokenResponse>
+{
+    private readonly IJwtService _jwtService;
+
+    public LogInUserCommandHandler(IJwtService jwtService)
+    {
+        _jwtService = jwtService;
+    }
+
+    public async Task<Result<AccessTokenResponse>> Handle(
+        LogInUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _jwtService.GetAccessTokenAsync(
+            request.Email,
+            request.Password,
+            cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure<AccessTokenResponse>(UserErrors.InvalidCredentials);
+        }
+
+        return new AccessTokenResponse(result.Value);
+    }
+}
