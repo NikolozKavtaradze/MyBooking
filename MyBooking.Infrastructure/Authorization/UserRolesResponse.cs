4eefe42cb11f05119c0d16959f4ea307d1d3cb4b
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-05 05:15:08 -0800

    Added AUthroization service, Custom Claims Transformation for Role Based authorization

---
+++ MyBooking.Infrastructure/Authorization/UserRolesResponse.cs
@@ -0,0 +1,9 @@
+﻿using MyBooking.Domain.Users;
+
+namespace MyBooking.Infrastructure.Authorization;
+
+public sealed class UserRolesResponse
+{
+    public Guid Id { get; set; }
+    public List<Role> Roles { get; init; } = [];
+}
