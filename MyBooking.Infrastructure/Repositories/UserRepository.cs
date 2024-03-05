6bd03eb6f1ae9b732623caabf4be52859b39974f
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-05 05:14:26 -0800

    Overriden add method

--- MyBooking.Infrastructure/Repositories/UserRepository.cs
+++ MyBooking.Infrastructure/Repositories/UserRepository.cs
@@ -10,4 +10,12 @@ internal sealed class UserRepository : Repository<User>, IUserRepository
         : base(dbContext)
     {
     }
+
+    public override void Add(User user)
+    {
+        foreach (var role in user.Roles)
+            DbContext.Attach(role);
+
+        DbContext.Add(user);
+    }
 }
