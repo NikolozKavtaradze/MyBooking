b2b972e6a34642839831bae0e38d2b70aa7714c1
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-05 05:13:05 -0800

    Ef Core configurations for Role, Permission and RolePermission Tables

---
+++ MyBooking.Infrastructure/Configurations/RoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBooking.Domain.Users;
+
+namespace MyBooking.Infrastructure.Configurations;
+
+internal class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("roles");
+
+        builder.HasKey(r => r.Id);
+
+        builder.HasMany(role => role.Users)
+            .WithMany(user => user.Roles);
+
+        builder.HasData(Role.Registered);
+    }
+}
