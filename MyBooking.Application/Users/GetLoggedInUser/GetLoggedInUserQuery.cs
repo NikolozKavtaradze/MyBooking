c605e2b99d0592d566fc8c966d3605491d3aebf3
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-03-05 05:12:06 -0800

    Added GetLoggerdInUser Query and Handler

---
+++ MyBooking.Application/Users/GetLoggedInUser/GetLoggedInUserQuery.cs
@@ -0,0 +1,5 @@
+﻿using MyBooking.Application.Abstractions.Messaging;
+
+namespace MyBooking.Application.Users.GetLoggedInUser;
+
+public sealed record GetLoggedInUserQuery : IQuery<UserResponse>;
---
+++ MyBooking.Application/Users/GetLoggedInUser/UserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBooking.Application.Users.GetLoggedInUser;
+
+public class UserResponse
+{
+    public Guid Id { get; init; }
+
+    public string Email { get; init; }
+
+    public string FirstName { get; init; }
+
+    public string LastName { get; init; }
+}
